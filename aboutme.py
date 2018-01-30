5f0ae222ca2be8fc0e4545106a97fe3aa2b0d578
Dinan Yin <Dinan.Yin@csiro.au>
2018-01-30 21:25:46 +1100

    new scripts

---
+++ aboutme.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+print('hello my name is Luc')
+
+print('i will draw you a dinosour')
+print('''
+               __
+              / _)
+     _/\/\/\_/ /
+   _|         /
+ _|  (  | (  |
+/__.-'|_|--|_|
+''')
