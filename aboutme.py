a380b5b22bdb3b28ac35f272de4baa0ce5525f12
Dinan Yin <Dinan.Yin@csiro.au>
2018-02-01 21:57:48 +1100

    refresher course for birthday math

--- aboutme.py
+++ aboutme.py
@@ -12,4 +12,34 @@ print('''
 /__.-'|_|--|_|
 ''')
 
-print(2018 - 2009)
+# 2018 - 2009 = 9
+# 2018 - 9 = 2009
+# 2009 + 9 = 2018
+# 9 + 2009 = 2018
+# 2009 + 10 = 2019
+
+# 2009 + 1 = 2010
+# 2010 -2009 = 1
+
+# 2010-
+
+
+# I have 9 apples, I got another 2 apples, now I have 11 apples
+# 9 + 2 = 11
+# I was born in 2009, I am 2 years old, what year is it? Answer: now it's 2011.
+# 2009 + 2 = 2011
+# Now it's 2011, I was born in 2009, how old am I? Answer: I am 2 years old.
+# 2011 - 2009 = 2
+# Now it's 2011, I am 2 years old, when was I born? Answer: I was born in 2009
+# 2011 - 2 = 2009
+
+# I was born in 2009, I am 20 years old, what year is it? Answer: it's now 2029
+# 2009 + 20 = 2029
+# I was born in 2009, I am 30 years old, what year is it? Answer: it's now 2039
+# 2009 + 30 = 2039
+
+birth_year = 2009
+print(2018 - birth_year)
+
+# I was born in 2009, I am 35 years old, what year is it? Let computer give me the answer.
+print(2009 + 35)
